ce5a742e26e8275a41ee7ac281a13411901e9cde
Anubhav <anubhav552006@gmail.com>
2026-07-08 20:08:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,28 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+API_KEY = "f500a7e58d36e1391083b33be63a4bc7"
+lat=42.5
+lon=0.00
+from twilio.rest import Client
+account_sid = "AC0b69f3b0bd0e50ffaf68650bee82b4d9"
+auth_token = "014fd195dda91166c1d6cc628d2243df"
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+import requests
+response = requests.get(url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}&cnt=4")
+response.raise_for_status()
+data = response.json()
+Rain = False
+for i in range (0,4):
+    if data["list"][i]["weather"][0]["id"] < 700 :
+        Rain = True
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+if Rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It is going to rain today ⛈, Make sure to bring an Umbrella☔",
+        from_='+16592745718',
+        to='+917979011500'
+    )
+    print(message.status)
